a1032d8150e44727bd6c9344f1d3ca89cfb5da75
Ilia Bosis <Iliab@hiskysat.com>
2021-11-03 13:47:00 +0200

    Very basic C app example

---
+++ hello/hello.c
@@ -0,0 +1,15 @@
+/********************************* includes **********************************/
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/******************************** constants **********************************/
+#define HELLO_MESSAGE "Hello world"
+
+/******************************** function definitions ***********************/
+int main(int argc, char *argv[])
+{
+    printf(HELLO_MESSAGE);
+    
+    return 0;
+)
